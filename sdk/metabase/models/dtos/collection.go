fce496fa23cb3daa23adc03b41dc61a07b3771af
Carlos Sánchez Páez <carlos.sanchez@feverup.com>
2025-04-27 21:00:27 +0200

    Add collection resource (#3)

---
+++ sdk/metabase/models/dtos/collection.go
@@ -0,0 +1,10 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package dtos
+
+type CollectionDTO struct {
+	Id       int    `json:"id"`
+	Name     string `json:"name"`
+	ParentId *int   `json:"parent_id"`
+}
---
+++ sdk/metabase/models/terraform/collection.go
@@ -0,0 +1,31 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package terraform
+
+import (
+	"github.com/csp33/terraform-provider-metabase/sdk/metabase/models/dtos"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"strconv"
+)
+
+type CollectionTerraformModel struct {
+	Id       types.String `tfsdk:"id"`
+	Name     types.String `tfsdk:"name"`
+	ParentId types.String `tfsdk:"parent_id"`
+}
+
+func CreateCollectionTerraformModelFromDTO(source *dtos.CollectionDTO) CollectionTerraformModel {
+	var parentId types.String
+	if source.ParentId == nil {
+		parentId = types.StringNull()
+	} else {
+		parentId = types.StringValue(strconv.Itoa(*source.ParentId))
+	}
+	return CollectionTerraformModel{
+		Id:       types.StringValue(strconv.Itoa(source.Id)),
+		Name:     types.StringValue(source.Name),
+		ParentId: parentId,
+	}
+
+}
