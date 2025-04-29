3cb20bb1b940280c2df83f0812b6f4d513fe0170
Carlos Sánchez Páez <karlossanpa@gmail.com>
2025-04-29 23:32:55 +0200

    wip

--- tools/init-metabase.sh
+++ tools/init-metabase.sh
@@ -2,28 +2,36 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
+echo "Waiting for Metabase to be ready..."
+while ! curl -s -o /dev/null -w "%{http_code}" http://localhost:3000/api/health | grep "200"; do
+  echo "Metabase not ready yet. Waiting 5 seconds..."
+  sleep 5
+done
+echo "Metabase is ready!"
 
-# 1. Get the setup-token
 echo "Getting the setup-token..."
-response_properties=$(curl --location 'http://localhost:3000/api/session/properties' -s)
+response_properties=$(curl 'http://localhost:3000/api/session/properties' -s)
 
 if [ $? -ne 0 ]; then
   echo "Error getting session properties."
   exit 1
 fi
 
-setup_token=$(echo "$response_properties" | jq -r '.setup-token')
+
+setup_token=$(echo "$response_properties" | jq -r '.["setup-token"]')
+
 
 if [ -z "$setup_token" ]; then
   echo "Could not extract the setup-token."
   exit 1
 fi
 
+
 echo "Setup-token obtained: $setup_token"
 
 # 2. Perform the setup
 echo "Performing the setup..."
-setup_response=$(curl --location 'http://localhost:3000/api/setup' \
+setup_response=$(curl -XPOST 'http://localhost:3000/api/setup' \
   --header 'Content-Type: application/json' \
   --data-raw "{
       \"user\": {
@@ -42,7 +50,7 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-session_token=$(echo "$setup_response" | jq -r '.token')
+session_token=$(echo "$setup_response" | jq -r '.["id"]')
 
 if [ -z "$session_token" ]; then
   echo "Could not extract the session token from the setup."
@@ -52,9 +60,9 @@ fi
 
 echo "Setup completed. Session token obtained: $session_token"
 
-# 3. Create the API Key
+## 3. Create the API Key
 echo "Creating the API Key..."
-api_key_response=$(curl --location 'http://localhost:3000/api/api-key' \
+api_key_response=$(curl -XPOST 'http://localhost:3000/api/api-key' \
   --header "X-Metabase-Session: $session_token" \
   --header 'Content-Type: application/json' \
   --data '{
